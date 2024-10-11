b6ba7864bc3e62745113a7034bd18d54f28931a4
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-11 18:08:19 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+// Shae Sullivan
+// 20643731
+// Programming Languages - Assignment
+// 17/09/24
 
 
-
-fn main() {
+fn main() { //decalre main method
     
 
-    fn product_of_four(a: i32, b: i32, c: i32, d: i32) -> i32 {
+    fn product_of_four(a: i32, b: i32, c: i32, d: i32) -> i32 { //declare function that takes 4 signed 32 bit integers -> declare result returned will be 32 bit signed integer
 
-        let result = a * b * c * d;
-        return result;
+        let result = a * b * c * d; //decalre variable result -> a*b*c*d parameters 
+        return result; //return result 
     }
 
-    product_of_four(31, 34, 65, 67);
+    product_of_four(31, 34, 65, 67); //call product_of_four function pass it -> 31,34,65,67
 
 
 
