a2b158a18b64cad63126efd3bee8b80c7a4cceee
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-11 17:59:19 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+
+
 fn main() {
-    println!("Hello, world!");
+    
+
+    fn product_of_four(a: i32, b: i32, c: i32, d: i32) -> i32 {
+
+        let result = a * b * c * d;
+        return result;
+    }
+
+    product_of_four(31, 34, 65, 67);
+
+
+
+
 }
