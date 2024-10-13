71abbca0b05aa90bd6d28ec56b45cdcae4b288bc
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-13 16:34:05 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() { //decalre main method
 
     fn product_of_four(a: i32, b: i32, c: i32, d: i32) -> i32 { //declare function that takes 4 signed 32 bit integers -> declare result returned will be 32 bit signed integer
 
-        let result = a * b * c * d; //decalre variable result -> a*b*c*d parameters 
+        let result: i32 = a * b * c * d; //decalre variable result -> a*b*c*d parameters 
         return result; //return result 
     }
 
